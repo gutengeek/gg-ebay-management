b78a05b42dba03acd20f5d6470c8224d5ff50a58
Alpha <alpha@Alphas-Mac-mini.local>
2020-08-04 17:36:30 +0700

    Update Posttypes.php

--- inc/Common/Posttypes.php
+++ inc/Common/Posttypes.php
@@ -25,6 +25,7 @@ class Posttypes {
 		}
 
 		static::register_ebay_account();
+		static::register_taxonomies();
 	}
 
 	/**
@@ -36,8 +37,8 @@ class Posttypes {
 		$labels = [
 			'name'               => esc_html__( 'eBay Account', 'ggem' ),
 			'singular_name'      => esc_html__( 'eBay Account', 'ggem' ),
-			'add_new'            => esc_html__( 'Add New eBay Account', 'ggem' ),
-			'add_new_item'       => esc_html__( 'Add New eBay Account', 'ggem' ),
+			'add_new'            => esc_html__( 'Add New Account', 'ggem' ),
+			'add_new_item'       => esc_html__( 'Add New Account', 'ggem' ),
 			'edit_item'          => esc_html__( 'Edit eBay Account', 'ggem' ),
 			'new_item'           => esc_html__( 'New eBay Account', 'ggem' ),
 			'all_items'          => esc_html__( 'eBay Accounts', 'ggem' ),
@@ -46,7 +47,7 @@ class Posttypes {
 			'not_found'          => esc_html__( 'No eBay Account found', 'ggem' ),
 			'not_found_in_trash' => esc_html__( 'No eBay Account found in Trash', 'ggem' ),
 			'parent_item_colon'  => '',
-			'menu_name'          => esc_html__( 'eBay Account', 'ggem' ),
+			'menu_name'          => esc_html__( 'eBay Management', 'ggem' ),
 		];
 
 		$labels = apply_filters( 'ggem_postype_paypal_labels', $labels );
@@ -55,40 +56,6 @@ class Posttypes {
 			apply_filters( 'ggem_ebay_post_type_parameters', [
 				'labels'              => $labels,
 				'supports'            => [ 'title', 'editor' ],
-				'public'              => false,
-				'hierarchical'        => false,
-				'exclude_from_search' => false,
-				'publicly_queryable'  => true,
-				'show_ui'             => true,
-				// 'show_in_menu'        => 'ggem',
-				'show_in_nav_menus'   => true,
-				'show_in_rest'        => true,
-				'map_meta_cap'        => true,
-				'has_archive'         => true,
-				'query_var'           => true,
-			] )
-		);
-
-		$labels2 = [
-			'name'               => esc_html__( 'Properties', 'opalestate-pro' ),
-			'singular_name'      => esc_html__( 'Property', 'opalestate-pro' ),
-			'add_new'            => esc_html__( 'Add New Property', 'opalestate-pro' ),
-			'add_new_item'       => esc_html__( 'Add New Property', 'opalestate-pro' ),
-			'edit_item'          => esc_html__( 'Edit Property', 'opalestate-pro' ),
-			'new_item'           => esc_html__( 'New Property', 'opalestate-pro' ),
-			'all_items'          => esc_html__( 'All Properties', 'opalestate-pro' ),
-			'view_item'          => esc_html__( 'View Property', 'opalestate-pro' ),
-			'search_items'       => esc_html__( 'Search Property', 'opalestate-pro' ),
-			'not_found'          => esc_html__( 'No Properties found', 'opalestate-pro' ),
-			'not_found_in_trash' => esc_html__( 'No Properties found in Trash', 'opalestate-pro' ),
-			'parent_item_colon'  => '',
-			'menu_name'          => esc_html__( 'Properties', 'opalestate-pro' ),
-		];
-
-		register_post_type( 'opalestate_property',
-			apply_filters( 'opalestate_property_post_type_parameters', [
-				'labels'              => $labels2,
-				'supports'            => [ 'title', 'editor', 'thumbnail', 'comments', 'author' ],
 				'public'              => true,
 				'has_archive'         => true,
 				'menu_position'       => 51,
@@ -96,9 +63,71 @@ class Posttypes {
 				'publicly_queryable'  => true,
 				'exclude_from_search' => false,
 				'query_var'           => true,
-				'hierarchical'        => false, // Hierarchical causes memory issues - WP loads all records!
+				'hierarchical'        => false,
 				'show_in_nav_menus'   => true,
 			] )
+		);
+	}
+
+	public static function register_taxonomies() {
+		if ( ! is_blog_installed() ) {
+			return;
+		}
+
+		register_taxonomy(
+			'ggem_server',
+			apply_filters( 'ggem_taxonomy_objects_server', [ 'ggem_ebay' ] ),
+			apply_filters(
+				'ggem_taxonomy_args_server',
+				[
+					'hierarchical' => true,
+					'label'        => __( 'Servers', 'ggem' ),
+					'labels'       => [
+						'name'              => __( 'Servers', 'ggem' ),
+						'singular_name'     => __( 'Server', 'ggem' ),
+						'menu_name'         => _x( 'Servers', 'Admin menu name', 'ggem' ),
+						'search_items'      => __( 'Search servers', 'ggem' ),
+						'all_items'         => __( 'All servers', 'ggem' ),
+						'parent_item'       => __( 'Parent server', 'ggem' ),
+						'parent_item_colon' => __( 'Parent server:', 'ggem' ),
+						'edit_item'         => __( 'Edit server', 'ggem' ),
+						'update_item'       => __( 'Update server', 'ggem' ),
+						'add_new_item'      => __( 'Add new server', 'ggem' ),
+						'new_item_name'     => __( 'New server name', 'ggem' ),
+						'not_found'         => __( 'No servers found', 'ggem' ),
+					],
+					'show_ui'      => true,
+					'query_var'    => true,
+				]
+			)
+		);
+
+		register_taxonomy(
+			'ggem_payment',
+			apply_filters( 'ggem_taxonomy_objects_payment', [ 'ggem_ebay' ] ),
+			apply_filters(
+				'ggem_taxonomy_args_payment',
+				[
+					'hierarchical' => true,
+					'label'        => __( 'Payments', 'ggem' ),
+					'labels'       => [
+						'name'              => __( 'Payments', 'ggem' ),
+						'singular_name'     => __( 'Payment', 'ggem' ),
+						'menu_name'         => _x( 'Payments', 'Admin menu name', 'ggem' ),
+						'search_items'      => __( 'Search payments', 'ggem' ),
+						'all_items'         => __( 'All payments', 'ggem' ),
+						'parent_item'       => __( 'Parent payment', 'ggem' ),
+						'parent_item_colon' => __( 'Parent payment:', 'ggem' ),
+						'edit_item'         => __( 'Edit payment', 'ggem' ),
+						'update_item'       => __( 'Update payment', 'ggem' ),
+						'add_new_item'      => __( 'Add new payment', 'ggem' ),
+						'new_item_name'     => __( 'New payment name', 'ggem' ),
+						'not_found'         => __( 'No payments found', 'ggem' ),
+					],
+					'show_ui'      => true,
+					'query_var'    => true,
+				]
+			)
 		);
 	}
 }
